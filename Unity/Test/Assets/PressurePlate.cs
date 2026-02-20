127e00c145c4521d8af59169d7bfe5ef2642f0a8
QiRuiQQ <86481730+RQ0392@users.noreply.github.com>
2026-02-20 16:27:55 -0700

    1
    1

---
+++ Unity/Test/Assets/PressurePlate.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag = "Player"):
+            {
+                Debug.Log(123123123)//
+            }
+    }
+}
